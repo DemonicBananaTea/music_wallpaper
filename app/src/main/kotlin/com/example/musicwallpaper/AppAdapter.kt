95b1cdeac164f89cc59b75f420dc7369fba735b8
bananam24 <bananam24@gmail.com>
2026-05-01 21:01:08 +0300

    fixing gui app picker

--- app/src/main/kotlin/com/example/musicwallpaper/AppAdapter.kt
+++ app/src/main/kotlin/com/example/musicwallpaper/AppAdapter.kt
@@ -1,8 +1,7 @@
 package com.example.musicwallpaper
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.CheckBox
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class AppAdapter(
@@ -13,8 +12,7 @@ class AppAdapter(
     class VH(val checkBox: CheckBox) : RecyclerView.ViewHolder(checkBox)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        val cb = LayoutInflater.from(parent.context)
-            .inflate(android.R.layout.simple_list_item_multiple_choice, parent, false) as CheckBox
+        val cb = CheckBox(parent.context)
         return VH(cb)
     }
 
@@ -23,6 +21,8 @@ class AppAdapter(
 
         holder.checkBox.text = item.label
         holder.checkBox.isChecked = item.selected
+
+        holder.checkBox.setOnCheckedChangeListener(null)
 
         holder.checkBox.setOnCheckedChangeListener { _, isChecked ->
             item.selected = isChecked
