5aa42007f04532fc56118c9db1266fcde5b19e47
bananam24 <bananam24@gmail.com>
2026-05-01 21:54:02 +0300

    fixing gui app picker

--- app/src/main/kotlin/com/example/musicwallpaper/AppSelectActivity.kt
+++ app/src/main/kotlin/com/example/musicwallpaper/AppSelectActivity.kt
@@ -1,6 +1,8 @@
 package com.example.musicwallpaper
 
 import android.content.pm.PackageManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
