a5ef210cc361e5cbf0fa36952f916089fcf812da
bananam24 <bananam24@gmail.com>
2026-05-02 13:14:07 +0300

    fixing gui app picker

--- app/src/main/kotlin/com/example/musicwallpaper/MediaSessionListener.kt
+++ app/src/main/kotlin/com/example/musicwallpaper/MediaSessionListener.kt
@@ -1,3 +1,10 @@
+package com.example.musicwallpaper
+
+import android.util.Log
+import android.content.ComponentName
+import android.content.Context
+import android.media.session.MediaSessionManager
+
 class MediaSessionReader(private val context: Context) {
 
     private val manager = context.getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager
